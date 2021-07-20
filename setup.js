36fe176450c4ff2e582a2d206e8ed6062fe08713
DiddySmooth <76023664+DiddySmooth@users.noreply.github.com>
2021-07-20 18:25:41 -0500

    3rd pass done islands still bad

--- setup.js
+++ setup.js
@@ -4,8 +4,8 @@ const container = document.createElement('div')
 container.classList.add("container")
 const map = document.querySelector(".map")
 console.log("setup")
-for(let j = 0; j< 40; j++){
-    for( let i = 0; i < 40; i++){
+for(let j = 0; j< 100; j++){
+    for( let i = 0; i < 100; i++){
         const square = document.createElement('div')
         square.classList.add("tile")
         column.append(square)
@@ -28,7 +28,6 @@ const cleanPass = () => {
 
 
 }
-
 
 const firstPass = () =>{
     const map1 = document.querySelectorAll(".tile")
@@ -77,9 +76,81 @@ const secondPass = () =>{
             }
         }
     }
-    console.log(map1[0].classList)
 }
 
+const thirdPass = () => {
+    const map1 = document.querySelectorAll(".tile")
+    
+    for(let i = 0; i < map1.length; i++){
+        if(i == 0){
+            rand = Math.floor(Math.random() * 2);
+            if(rand == 0){
+                map1[i].classList.add("water")
+            }
+            else{
+                map1[i].classList.add("land")
+            }
+        }
+        else if(i != 0 && i <100){
+            if(map1[i-1].classList[1] == "water"){
+                rand = Math.floor(Math.random() * 5);
+                if(rand == 0){
+                    map1[i].classList.add("land")
+                }
+                else{
+                    map1[i].classList.add("water")
+                }
+            }
+            else if(map1[i-1].classList[1] == "land"){
+                rand = Math.floor(Math.random() * 5);
+                if(rand == 0){
+                    map1[i].classList.add("water")
+                }
+                else{
+                    map1[i].classList.add("land")
+                }
+            }
+        }
+        else{
+            if(map1[i-1].classList[1] == "water" && map1[i-100].classList[1] == 'water'){
+                rand = Math.floor(Math.random() * 20);
+                if(rand == 0){
+                    map1[i].classList.add("land")
+                }
+                else{
+                    map1[i].classList.add("water")
+                }
+            }
+            else if(map1[i-1].classList[1] == "land"  && map1[i-100].classList[1] == 'land'){
+                rand = Math.floor(Math.random() * 10);
+                if(rand == 0){
+                    map1[i].classList.add("water")
+                }
+                else{
+                    map1[i].classList.add("land")
+                }
+            }
+            else if(map1[i-1].classList[1] == "water"){
+                rand = Math.floor(Math.random() * 6);
+                if(rand == 0){
+                    map1[i].classList.add("land")
+                }
+                else{
+                    map1[i].classList.add("water")
+                }
+            }
+            else if(map1[i-1].classList[1] == "land"){
+                rand = Math.floor(Math.random() * 4);
+                if(rand == 0){
+                    map1[i].classList.add("water")
+                }
+                else{
+                    map1[i].classList.add("land")
+                }
+            }
+        }
+    }
+}
 
 document.querySelector(".next").addEventListener("click", function() {
     cleanPass()
@@ -90,5 +161,8 @@ document.querySelector(".next").addEventListener("click", function() {
     }
     else if(state == 2){
         secondPass()
+    }
+    else if(state == 3){
+        thirdPass()
     }
 })
